95eb6ebbde717738e75d2f7ced269a5296524e87
andy chiang <runlikeben@yahoo.com>
2012-04-07 15:18:44 -0500

    building up graph

--- graph.rb
+++ graph.rb
@@ -1,8 +1,24 @@
 require 'node'
 require 'arc'
 
+# class Graph < Hash # of String, Node
 class Graph
 
+  def initialize
+    @nodes = Hash.new
+  end
 
+  def nodes
+    @nodes.values
+  end
+
+  def addNode( n )
+    raise TypeError "Graphs only contain nodes" unless n.is_a? Node
+    @nodes.has_key?( n.name) ? @nodes.store( n.name, n ) : @nodes[n.name]
+  end
+
+  def addArc( src, dest, weight )
+    # something.
+  end
 
 end
--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -7,11 +7,30 @@ describe Graph do
     @testGraph = Graph.new
   end
 
+  describe "#nodes" do
+    it "returns an array of nodes" do
+      @testGraph.nodes.should be_an_instance_of Array
+    end
+  end
+
+  it "has a method for adding nodes" do
+    @testGraph.respond_to?( :addNode ).should be_true
+  end
+
+  it "has a method for adding arcs" do
+    @testGraph.respond_to?( :addArc ).should be_true
+  end
+
   describe "#new" do
     it "creates a new Graph instance" do
       @testGraph.should be_an_instance_of Graph
     end
+    it "creates an empty Graph" do
+      @testGraph.should have(0).nodes
+    end
   end
+  
+  
 
 
 end
