1b007def5ab275d399da46e16c463b88bb44ff6b
andy chiang <runlikeben@yahoo.com>
2012-04-07 10:01:20 -0500

    node class

---
+++ node.rb
@@ -0,0 +1,6 @@
+class Node
+
+  def initialize( nm )
+  end
+
+end
---
+++ spec/node_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'node'
+
+describe Node do
+
+  before :each do
+    @testnode = Node.new "Test"
+  end
+
+  describe "#new" do
+    it "takes one parameter and returns a Node instance" do
+      @testnode.should be_an_instance_of Node
+    end
+  end
+
+
+end
