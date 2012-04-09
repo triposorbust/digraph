7f4cf0692f0c8c9303e40feaa1dc16a2e8a894e8
andy chiang <runlikeben@yahoo.com>
2012-04-08 20:14:42 -0500

    stub out routeCounter

---
+++ graph+routeCounter.rb
@@ -0,0 +1,21 @@
+require 'node+adjacencyHash'
+require 'graph'
+
+class Graph
+
+  def countRoutesWithStops( sourceName,
+                            destinationName,
+                            stops,
+                            strict = false )
+    return
+  end
+
+  def countRoutesWithMaxDist( sourceName,
+                              destinationName,
+                              maxDist )
+    return
+  end
+
+  private
+
+end
---
+++ spec/graph+routeCounter_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'graphFactory'
+require 'graph+routeCounter'
+
+describe Graph do
+
+
+end
+
+describe_internally Graph do
+
+
+end
